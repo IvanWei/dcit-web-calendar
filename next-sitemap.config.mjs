4f5b6860d9cd042e001a8d13ad473bd50fb6a155
Ivan Wei <scorpionseed@gmail.com>
2023-06-06 15:23:05 +0800

    chore: update next sitemap config

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,7 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+
 export default {
   siteUrl: process.env.SITE_URL || 'http://localhost:8080',
+  changefreq: 'daily',
   generateRobotsTxt: true,
   outDir: './out',
   sitemapSize: 7000,
-  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    transformRobotsTxt: async (config, robotsTxt) => {
+      return robotsTxt.replace('sitemap', 'sitemap-0');
+    },
+    // additionalSitemaps: [`${process.env.SITE_URL || 'http://localhost:8080'}/sitemap-0.xml`],
+  },
 }

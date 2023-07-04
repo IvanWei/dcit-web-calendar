1eec798a1bf7ba230fbcce22bd79dc113554d139
Ivan Wei <scorpionseed@gmail.com>
2023-07-04 09:52:30 +0800

    feature: add publish version to footbar

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={FooterStyle['footer-basic']}>
       Copyright &copy; {new Date().getFullYear()} DCIT team (維護者：{' '}
-      <Link href='mailto:contact@ivanwei.co'>Wei Hong-Lin</Link>) 保留一切權利。
+      <Link href='mailto:contact@ivanwei.co'>Wei Hong-Lin</Link>) 保留一切權利。 (最後更新: {process.env.NEXT_PUBLISH_VERSION})
     </footer>
   );
 };
